859ee9f14d65c6d686ff59d66fa0c6b7bda54995
Shane Canon <scanon@lbl.gov>
2014-12-08 16:44:08 -0800

    Added missing files and fixed rebuild script

--- mydock/rebuild-head.sh
+++ mydock/rebuild-head.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 VERS=$(cat ../VERSION)
+BUILDDIR=builddir
 echo $VERSION
 
 echo "Creating tar file"
 cd ../../
-tar czf ~/rpmbuild/SOURCES/docker-head.tar.gz docker
+mkdir $BUILDDIR
+mkdir $BUILDDIR/SOURCES
+mkdir $BUILDDIR/SPECS
+tar czf $BUILDDIR/SOURCES/docker-head.tar.gz docker
 cd docker/mydock
 
-cp workarounds.diff ~/rpmbuild/SOURCES/
+cp docker.sysconfig docker-storage.sysconfig docker.sysvinit workarounds.diff ../../$BUILDDIR/SOURCES/
 
-rpmbuild -ba ./docker-io-head.spec || exit
-rpm -Uhv --force ~/rpmbuild/RPMS/x86_64/docker-io-$(cat ../VERSION|sed 's/-//')-*nersc*rpm || exit
-
-/etc/init.d/docker restart
+rpmbuild --define '_topdir '`cd ../../;pwd`/${BUILDDIR} -ba ./docker-io-head.spec || exit
+#rpm -Uhv --force ~/rpmbuild/RPMS/x86_64/docker-io-$(cat ../VERSION|sed 's/-//')-*nersc*rpm || exit
+#/etc/init.d/docker restart
 
 
